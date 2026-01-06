504cbb512b9dfeab76a9524328aee0c27b1b195b
Errong Leng <errong.leng@gmail.com>
2026-01-05 22:18:12 -0800

    init landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
-export default async function Home() {
-  return <></>;
+import Image from "next/image";
+
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <Image
+        src="/gemini_guess.png"
+        alt="Gemini Guess Logo"
+        width={500}
+        height={500}
+        priority
+      />
+    </main>
+  );
 }
